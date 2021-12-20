238cf97072708093b89fa7e9269f90342ae5266b
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-20 16:52:49 +0100

    navbar and categories created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
+import Navbar from './Components/Navbar'
 function App() {
   return (
     <div className="App">
-      
+      <Navbar categories={[`business`, `entertainment`, `general`, `health`, `science`, `sports`,` technology`]}/>
     </div>
   )
 }
-
 export default App
