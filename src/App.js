bb289e54d06fafe7ab5ba6d1d418a3728c0848d8
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-20 20:12:56 +0100

    Jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Jumbotron from './components/Jumbotron'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Jumbotron/>
+      </Container>
     </div>
   )
 }
