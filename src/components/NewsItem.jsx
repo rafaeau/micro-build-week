b3d1b7a17ac6c6237b77612b5b7479a1580d9b57
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-20 16:16:43 +0100

    date formatted

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { parseISO, format } from 'date-fns'
 
 export default function NewsItem({ title, description, url, date, author, content }) {
     return (
         <div className="ml-4">
             <div>
                 <h1 className="mb-0 post-title" style={{ fontWeight: "bolder" }}>{title}</h1>
-                <p className="mb-2 text-muted posted-date">{date} by <a className="post-author" href={url}>{author}</a></p>
+                <p className="mb-2 text-muted posted-date"> {format(parseISO(date), 'MMMM do yyyy | HH:mm')} by <a className="post-author" href={url}>{author}</a></p>
+
 
 
                 <p className="post-description">{description}</p>
