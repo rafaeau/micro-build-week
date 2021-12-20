0fd361926d535410d014a890d182d738629b155e
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-20 12:20:36 +0100

    posts styling is done

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function MyPosts() {
-    return (
-        <div>
-            Hello
-        </div>
-    )
+export default class MyPosts extends Component {
+    render() {
+        return (
+            <div className="ml-4">
+                <div>
+                    <h1 className="about-sec">From the Firehose</h1>
+                    <hr></hr>
+                </div>
+                <div>
+                    <h1 className="mb-0" style={{ fontWeight: "bolder" }}>Sample blog post</h1>
+                    <p className="mb-2 text-muted">January 1, 2014 by <a href="#">Mark</a></p>
+
+
+                    <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+                </div>
+            </div>
+        )
+    }
 }
