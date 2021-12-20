b41ab35f0c7744df11c82a6172895a63fed213b7
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-20 16:11:36 +0100

    updated

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -8,7 +8,7 @@ export default function NewsList() {
 
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get('https://newsapi.org/v2/everything?q=tesla&from=2021-11-20&sortBy=publishedAt&apiKey=48fa8b968da1414895a50b68a45ab365')
+            const response = await axios.get('https://newsapi.org/v2/everything?q=football&apiKey=48fa8b968da1414895a50b68a45ab365')
             console.log(response)
             setArticles(response.data.articles)
         }
