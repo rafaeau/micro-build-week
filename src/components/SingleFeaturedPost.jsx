9c1b9ddc37d39800d9e714af55d5882b77adaf80
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-21 10:24:57 +0100

    updated

--- src/components/SingleFeaturedPost.jsx
+++ src/components/SingleFeaturedPost.jsx
@@ -23,18 +23,23 @@ class SingleFeaturedPost extends Component {
     render() {
         return (
             <div nameClass="row col-md-6">
-            <Card orientation="horizontal" nameClass="flex-md-row mb-4 box-shadow h-md-250">
-                <Card.Body nameClass="col-8 d-flex flex-column align-items-start">
-                <strong nameClass="d-inline-block mb-2 text-primary">World</strong>
-                        <h3 nameClass="mb-0">
-                            <h2>Featured post</h2>
-                        </h3>
-                        <div nameClass="mb-1 text-muted">Nov 12</div>
-                        <p nameClass="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#">Continue reading</a>
-                </Card.Body>
-                <Card.Img width="200px" height="250px" nameClass="col-4 card-img-right flex-auto d-none d-md-block" src="http://placehold.it/200x250"/>
-            </Card>
+                <Card orientation="horizontal" nameClass="flex-md-row mb-4 box-shadow h-md-250">
+                    <Card.Body nameClass="col-8 d-flex flex-column align-items-start" style={{ display: "flex" }}>
+                        <div>
+                            <strong nameClass="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 nameClass="mb-0">
+                                <h2>Featured post</h2>
+                            </h3>
+                            <div nameClass="mb-1 text-muted">Nov 12</div>
+                            <p nameClass="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#">Continue reading</a>
+                        </div>
+                        <div>
+                            <Card.Img width="200px" height="250px" nameClass="col-4 card-img-right flex-auto d-none d-md-block" src="http://placehold.it/200x250" />
+                        </div>
+                    </Card.Body>
+
+                </Card>
             </div>
         )
     }
