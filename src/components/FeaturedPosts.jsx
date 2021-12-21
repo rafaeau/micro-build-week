4cadef9935d95d5ed597668aa5acce61441b882a
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-21 19:03:53 +0100

    Featured posts working

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -3,8 +3,8 @@ import SingleFeaturedPost from './SingleFeaturedPost'
 const FeaturedPosts = () => {
     return (
         <div className="my-5 d-flex justify-content-around">
-        <SingleFeaturedPost/>
-        <SingleFeaturedPost/>
+        <SingleFeaturedPost article="2"/>
+        <SingleFeaturedPost article="4"/>
         </div>
     )
 }
--- src/components/SingleFeaturedPost.jsx
+++ src/components/SingleFeaturedPost.jsx
@@ -1,8 +1,10 @@
 import { Component } from 'react'
 import { Card } from 'react-bootstrap'
+import { parseISO, format } from 'date-fns'
 
 class SingleFeaturedPost extends Component {
     state = {
+        loading: true,
         comments: []
     }
 
@@ -11,7 +13,8 @@ class SingleFeaturedPost extends Component {
             let response = await fetch("https://newsapi.org/v2/top-headlines?country=au&apiKey=f7c47b6922c6434eb7afbb73428ffb90")
             let comments = await response.json()
             this.setState({
-                comments: comments.articles
+                comments: comments.articles,
+                loading: false
             })
             console.log(this.state.comments)
             console.log(this.state.comments[3].content)
@@ -22,24 +25,28 @@ class SingleFeaturedPost extends Component {
 
     render() {
         return (
-            <div nameClass="col-md-6">
-                <Card orientation="horizontal" nameClass="flex-md-row mb-4 box-shadow h-md-250">
-                    <Card.Body nameClass="row d-flex flex-column align-items-start" style={{ display: "flex", justifyContent: "around" }}>
-                        <div nameClass="col-8">
-                            <strong nameClass="d-inline-block mb-2 text-primary">World</strong>
-                            <h3 nameClass="mb-0">
-                                <h2>Featured post</h2>
-                            </h3>
-                            <Card.Text muted>Nov 12</Card.Text>
-                            <p nameClass="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#">Continue reading</a>
-                        </div>
-                        <div nameClass="col-4">
-                            <Card.Img width="150px" height="200px" nameClass="card-img-right flex-auto d-none d-md-block" src="http://placehold.it/200x150" />
-                        </div>
-                    </Card.Body>
-
-                </Card>
+            <div className="col-md-6">
+            {
+                this.state.loading ? "loading" :
+                        <Card orientation="horizontal" className="flex-md-row mb-4 box-shadow">
+                            <Card.Body className="row d-flex flex-column align-items-start" style={{ display: "flex", justifyContent: "around" }}>
+                                <div className="col-8">
+                                    <strong className="d-inline-block mb-2 text-primary">Australia</strong>
+                                    <h3 className="mb-0">
+                                        <h5 id="featuredTitle">{this.state.comments[this.props.article].title}</h5>
+                                    </h3>
+                                    <Card.Text muted>{format(parseISO(this.state.comments[this.props.article].publishedAt), 'dd/mm/yyyy HH:mm')}</Card.Text>
+                                    <p className="card-text mb-auto"><div id="featuredContent" dangerouslySetInnerHTML={{ __html: this.state.comments[this.props.article].content }}></div></p>
+                                    <a href="#">Continue reading</a>
+                                </div>
+                            </Card.Body>
+                            <div className="col-4">
+                            <Card.Img width="300px" height="300px" style={{objectFit: "cover"}}
+                                className="card-img-right flex-auto d-none d-md-block"
+                                src={this.state.comments[this.props.article].urlToImage} />
+                            </div>
+                        </Card>
+            }
             </div>
         )
     }
