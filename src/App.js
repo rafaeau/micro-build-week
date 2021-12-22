c4481ec45b24fab713ef04ee5523a8e6080fab45
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-22 14:58:13 +0100

    Header updated

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Carousel from './components/Carousel'
 import FeaturedPosts from './components/FeaturedPosts'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-
+import Navbar from './components/Navbar'
+import Header  from './components/Header'
 function App() {
   return (
+     
       <Container>
-        <Navbar categories={[`Business`, `Entertainment`, `General`, `Health`, `Science`, `Sports`,` Technology`]}/>
+         <Navbar  />
+         <Header categories={[`Business`, `Entertainment`, `General`, `Health`, `Science`, `Sports`,` Technology`]} />
         <Carousel/>
         <FeaturedPosts/>
       </Container>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,27 +3,12 @@
 //apikey = 87836ad8185b41b5bcf72d32081e2c63
 import {Component} from 'react'
 import Search from './Search'
-export default class Navbar extends Component{
-        state={
-            category:this.props.categories[0],
-        }
 
-        componentDidUpdate= async () => {
-            try {
-                let response = await fetch("https://newsapi.org/v2/everything?q=" + this.state.category + "&from=2021-11-22&sortBy=publishedAt&apiKey=87836ad8185b41b5bcf72d32081e2c63")
-                let news = await response.json()
-                console.log(news)
-                
-            }
-            catch(err) {
-                console.log(err)
-            }
-        }
-
+class Navbar extends Component{
     render(){
         return(
             <>
-                <div className="container mt-3">
+                <div className="container-fluid mt-3">
                     <div className ="row flex-nowrap pl-0  align-items-center">
                         <div className="col-4 pt-1 ">
                            <a className = 'text-muted' href='' style={{paddingLeft:'0px', alignItems:'flex-start'}}>Subscribe</a> 
@@ -32,28 +17,14 @@ export default class Navbar extends Component{
                             The Strive Times
                         </div>
                         <div className="col-4">
-                            {<Search />}
-                           <button className='btn btn-sm btn-outline-secondary'>
+                           <div>{<Search />} </div>
+                           <button className='btn btn-sm btn-outline-secondary ml-auto'>
                                Sign up
                            </button>
                         </div>
                     </div>
-                    <hr />
-                    <div className="container">
-                        <div className="row flex-nowrap justify-content-between" id=''>
-                        {
-                            Object.values(this.props.categories).map((category)=>{
-                                return (
-                                    <ul style={{listStyleType:'none', cursor:'pointer'}}>
-                                        <li onClick={()=>this.setState({category})} className='p-2 text-muted border-none'>{category}</li>
-                                    </ul>
-                                )
-                            })
-                        }
-                        </div>
-                    </div>
-                    <hr />
                 </div>
             </>
         )
     }}
+export default Navbar
