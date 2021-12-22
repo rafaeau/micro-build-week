229dc499fef37c006d5bb144c6ea71b7f72dac59
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-22 14:44:48 +0100

    Jumbotron and featured posts all finished

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -2,9 +2,13 @@ import SingleFeaturedPost from './SingleFeaturedPost'
 
 const FeaturedPosts = () => {
     return (
-        <div className="my-5 d-flex justify-content-around">
-        <SingleFeaturedPost article="3" region="World" color="rgb(0,123,255)"/>
-        <SingleFeaturedPost article="6" region="Australia" color="rgb(40,167,69)"/>
+        <div className="my-3 d-flex justify-content-around">
+            <div className='mr-4'>
+                <SingleFeaturedPost article="3" region="World" color="rgb (0,123,255)" />
+            </div>
+            <div>
+                <SingleFeaturedPost article="6" region="Australia" color="rgb (40,167,69)" />
+            </div>
         </div>
     )
 }
--- src/components/SingleFeaturedPost.jsx
+++ src/components/SingleFeaturedPost.jsx
@@ -25,16 +25,16 @@ class SingleFeaturedPost extends Component {
 
     render() {
         return (
-            <div className="col-md-6">
+            <>
                 {
                     this.state.loading ? "loading" :
-                        <Card className="flex-md-row mb-4 box-shadow h-md-250">
+                        <Card className="flex-md-row box-shadow h-md-250">
                             <Card.Body className="d-flex flex-column align-items-start">
-                                <strong className="d-inline-block mb-2" style={{color: this.props.color}}>{this.props.region}</strong>
+                                <strong className="d-inline-block mb-2" style={{ color: this.props.color }}>{this.props.region}</strong>
                                 <h3 className="mb-0">
                                     <h5 id="featuredTitle">{this.state.comments[this.props.article].title}</h5>
                                 </h3>
-                                <Card.Text muted style={{color: 'rgb(140, 140, 140'}}>{format(parseISO(this.state.comments[this.props.article].publishedAt), 'dd/mm/yyyy HH:mm')}</Card.Text>
+                                <Card.Text muted style={{ color: 'rgb(140, 140, 140' }}>{format(parseISO(this.state.comments[this.props.article].publishedAt), 'dd/mm/yyyy HH:mm')}</Card.Text>
                                 <p className="card-text mb-auto"><div id="featuredContent" dangerouslySetInnerHTML={{ __html: this.state.comments[this.props.article].content }}></div></p>
                                 <a href="#">Continue reading</a>
                             </Card.Body>
@@ -45,7 +45,7 @@ class SingleFeaturedPost extends Component {
                                 src={this.state.comments[this.props.article].urlToImage} />
                         </Card>
                 }
-            </div>
+            </>
         )
     }
 }
