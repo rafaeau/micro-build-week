7d5fd62b2df24c999980d0e87fe785d54894f907
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-22 16:48:41 +0100

    Revert "Merge remote-tracking branch 'origin/navbarAndCategories' into test-footer-1"
    This reverts commit 5618d5158b8c266aea3a42d17b5d2f4d4af252b5, reversing
    changes made to 92889bfb2826bd48297d0809ab6b477b8ec9d4bc.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { Container } from 'react-bootstrap'
 import MyFooter from './components/MyFooter'
 import MainPosts from './MainPosts'
+import { Container } from 'react-bootstrap'
 import Carousel from './components/Carousel'
 import FeaturedPosts from './components/FeaturedPosts'
-import Navbar from './components/Navbar'
-import Header  from './components/Header'
+
+
+
 
 function App() {
   return (
-     <>
+    <>
       <Container>
-         <Navbar  />
-         <Header categories={[`Business`, `Entertainment`, `General`, `Health`, `Science`, `Sports`,` Technology`]} />
-         <hr/>
-        <Carousel/>
-        <FeaturedPosts/>
+        <Carousel />
+        <FeaturedPosts />
       </Container>
       <MainPosts />
       <MyFooter />
