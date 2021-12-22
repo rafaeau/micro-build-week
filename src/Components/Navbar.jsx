b25a85b52f0c8144d44cbc46c7c43ce4d09a4999
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-22 13:53:28 +0100

    searchbar implemented

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,16 @@
 //Categories -> Sefa
 //apikey = 87836ad8185b41b5bcf72d32081e2c63
 import {Component} from 'react'
+import Search from './Search'
 export default class Navbar extends Component{
         state={
-            category:this.props.categories[0]
+            category:this.props.categories[0],
+            
         }
-         componentDidUpdate= async () => {
+
+        componentDidUpdate= async () => {
             try {
-                let response = await fetch("https://newsapi.org/v2/everything?q=" + this.state.category + "&from=2021-11-21&sortBy=publishedAt&apiKey=87836ad8185b41b5bcf72d32081e2c63")
+                let response = await fetch("https://newsapi.org/v2/everything?q=" + this.state.category + "&from=2021-11-22&sortBy=publishedAt&apiKey=87836ad8185b41b5bcf72d32081e2c63")
                 let news = await response.json()
                 console.log(news)
                 
@@ -17,7 +20,7 @@ export default class Navbar extends Component{
                 console.log(err)
             }
         }
-       
+
     render(){
         return(
             <>
@@ -30,13 +33,7 @@ export default class Navbar extends Component{
                             The Strive Times
                         </div>
                         <div className="col-4">
-                           <a href='#' className='text-muted'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" 
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" 
-                            stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle>
-                            <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
-                            </svg>
-                           </a>
+                            {<Search />}
                            <button className='btn btn-sm btn-outline-secondary'>
                                Sign up
                            </button>
