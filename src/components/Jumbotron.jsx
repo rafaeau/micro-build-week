5dcd896016868896433b50105648f12617290f3c
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-23 04:14:39 +0100

    updated

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -9,7 +9,7 @@ class Jumbotron extends Component {
 
     componentDidMount = async () => {
         try {
-            let response = await fetch("https://newsapi.org/v2/top-headlines?country=au&apiKey=f7c47b6922c6434eb7afbb73428ffb90")
+            let response = await fetch("https://newsapi.org/v2/everything?q=robert-Lewandowski&top-headlines?country=au&apiKey=f7c47b6922c6434eb7afbb73428ffb90")
             let comments = await response.json()
             this.setState({
                 comments: comments.articles,
@@ -32,13 +32,13 @@ class Jumbotron extends Component {
                     <div className='d-flex'>
                         <div className="col-6 ml-3">
                             <h1 id="jumboTitle"><i><b>{this.state.comments[this.props.article].title}</b></i></h1>
-                            
+
                             <div id="jumboContent" dangerouslySetInnerHTML={{ __html: this.state.comments[this.props.article].content }}></div>
-                            
+
                             <p className='pt-3'><b>Continue reading...</b></p>
                         </div>
                         <div className="col-6 mr-3">
-                        <img className="card-img-right flex-auto d-none d-lg-block"
+                            <img className="card-img-right flex-auto d-none d-lg-block"
                                 alt="Thumbnail"
                                 style={{ width: '525px', height: '250px', objectFit: 'cover' }}
                                 src={this.state.comments[this.props.article].urlToImage} />
